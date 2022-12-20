3d0acdaacd04b81ce8ce0eeb540f2d6499b50cbc
Miguel Beignon <miguel@twic.pics>
2022-12-20 21:31:01 +0100

    Fixes bot sample.

--- src/components/TwicBot.vue
+++ src/components/TwicBot.vue
@@ -74,6 +74,7 @@
           <TwicImg
             src="https://assets.twicpics.com/demo/@twicpics-components/bot/robot.jpg"
             bot="cover=400x400"
+            preTransform="cover=400x300"
             :anchor="actualConfig.anchor"
           />
           <span> Bot will override anchor="{{ actualConfig.anchor }}"</span>
@@ -89,7 +90,7 @@ export default {
     return {
       config: [
         {
-          anchor: undefined,
+          anchor: "center",
           preTransform: "flip=x",
           ratio: "4/3",
         },
