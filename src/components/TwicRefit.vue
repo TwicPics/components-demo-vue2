d24464138801ec21e068ce7d4f4ae2593729722e
Miguel Beignon <mbgspcii@gmail.com>
2023-07-13 14:51:07 +0200

    Components version 0.20.0.

--- src/components/TwicRefit.vue
+++ src/components/TwicRefit.vue
@@ -3,39 +3,44 @@
     <TwicWrapper filename="src/components/TwicRefit.vue">
       <TwicAbstract title="refit property">
         <p>
-          <strong>
-            The <dfn>refit</dfn> property resizes the image while maximizing the
-            area occupied by the main object (or objects) found within said
-            image.
-          </strong>
-        </p>
-        <p>
-          To enable <dfn>refit</dfn>, simply add the <Code>refit</Code> property
-          to the <Code>TwicImg</Code> component like in
-          <Code>&lt;TwicImg src="image.jpg" refit/&gt;</Code>
-        </p>
-        <p>
-          By default, the subject will be placed at the center of the resulting
-          image but
-          <strong>it is possible to align the subject</strong> with a given
-          border by specifying an
-          <a href="/anchor" target="_blank" rel="noreferrer"> anchor </a>
-          .
-        </p>
-        <p>
-          Also by default, the subject will touch the borders of the resulting
-          image. This can be avoided by <strong>adding padding</strong> like in
-          <Code>&lt;TwicImg src="image.jpg" refit="5p"/&gt;</Code>.
-        </p>
-        <p>
-          <a
-            href="https://www.twicpics.com/blog/discover-the-refit-transformation?utm_source=github&utm_medium=organic&utm_campaign=components#span-classexperimentalachromatopsiaspan"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Want to see more examples?
-          </a>
-        </p>
+        <strong>
+          The <dfn>refit</dfn> property:
+        </strong>
+      </p>
+      <ul>
+        <li>
+          reframes the image to maximize the area occupied by the main object(s) while respecting <a href="/ratio" target="_blank" rel="noreferrer"> ratio </a> in <TwicCode>cover</TwicCode> <a href="/mode" target="_blank" rel="noreferrer"> mode </a>.
+        </li>
+        <li>
+          crops the image as close as possible to the main object(s) in <TwicCode>contain</TwicCode> <a href="/mode" target="_blank" rel="noreferrer"> mode </a>.
+        </li>
+      </ul>
+      <p>
+        To enable <dfn>refit</dfn>, simply add the <TwicCode>refit</TwicCode> property to the <TwicCode
+          >TwicImg</TwicCode
+        > component like in
+        <TwicCode>&lt;TwicImg src="image.jpg" refit/&gt;</TwicCode>
+      </p>
+      <p>
+        By default, the subject will be placed at the center of the resulting image but
+        <strong>it is possible to align the subject</strong> with a given border by specifying an
+        <a href="/anchor" target="_blank" rel="noreferrer"> anchor </a>
+        .
+      </p>
+      <p>
+        Also by default, the subject will touch the borders of the resulting image. This can be
+        avoided by <strong>adding padding</strong> like in
+        <TwicCode>&lt;TwicImg src="image.jpg" refit="5p"/&gt;</TwicCode>.
+      </p>
+      <p>
+        <a
+          href="https://www.twicpics.com/blog/discover-the-refit-transformation?utm_source=github&utm_medium=organic&utm_campaign=components#span-classexperimentalachromatopsiaspan"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Want to see more examples?
+        </a>
+      </p>
       </TwicAbstract>
       <div class="twic-testing-container">
         <button class="twic-button" @click="updateIndice">
@@ -44,7 +49,7 @@
       </div>
       <div class="twic-grid">
         <div class="twic-item">
-          <TwicImg :src="IMG_URL" :mode="actualConfig.mode" />
+          <TwicImg v-if="show" :src="IMG_URL" :mode="actualConfig.mode" />
           <span> Original image + mode="{{ actualConfig.mode }}" </span>
         </div>
         <div class="twic-item">
@@ -63,11 +68,10 @@
             + mode='{{ actualConfig.mode }}'
           </span>
         </div>
-
         <div class="twic-item">
           <TwicImg
             :src="IMG_URL"
-            anchor="top"
+            anchor="left"
             :mode="actualConfig.mode"
             ratio="4/3"
             :refit="actualConfig.refit"
@@ -78,13 +82,13 @@
                 ? "refit"
                 : "refit=''" + actualConfig.refit + "''"
             }}
-            + mode='{{ actualConfig.mode }}' + anchor="top"</span
+            + mode='{{ actualConfig.mode }}' + anchor="left"</span
           >
         </div>
         <div class="twic-item">
           <TwicImg
             :src="IMG_URL"
-            anchor="bottom"
+            anchor="right"
             :mode="actualConfig.mode"
             ratio="4/3"
             :refit="actualConfig.refit"
@@ -95,7 +99,7 @@
                 ? "refit"
                 : "refit=''" + actualConfig.refit + "''"
             }}
-            {{ actualConfig.mode }}" + anchor="bottom"</span
+            {{ actualConfig.mode }}" + anchor="right"</span
           >
         </div>
       </div>
@@ -126,7 +130,8 @@ export default {
         },
       ],
       indiceConfig: 0,
-      IMG_URL: `components/refit/yellow-combi.jpg`,
+      IMG_URL: `components/refit/dog-looking-water.jpg`,
+      show: true,
     };
   },
   computed: {
@@ -136,7 +141,9 @@ export default {
   },
   methods: {
     updateIndice() {
+      setTimeout(() => (this.show = false))
       this.indiceConfig = (this.indiceConfig + 1) % this.config.length;
+      setTimeout(() => (this.show = true))
     },
   },
 };
